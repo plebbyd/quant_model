f419dac6cec015799c1d22ec31c760b22ee3a7f4
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2023-08-03 12:07:42 -0500

    Add files via upload

--- quant_inf.py
+++ quant_inf.py
@@ -4,6 +4,7 @@ from pycoral.utils import edgetpu
 from pycoral.utils import dataset
 from pycoral.adapters import common
 from pycoral.adapters import classify
+import numpy as np
 
 # Specify the TensorFlow model, labels, and image
 script_dir = pathlib.Path(__file__).parent.absolute()
@@ -14,10 +15,13 @@ interpreter = edgetpu.make_interpreter(model_file)
 interpreter.allocate_tensors()
 
 # Resize the image
-row = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.8]
+rows = np.array([[0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.8],
+        [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.8],
+        [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.8]
+        ])
 
 # Run an inference
-common.set_input(interpreter, row)
+common.set_input(interpreter, rows)
 interpreter.invoke()
 classes = classify.get_scores(interpreter)
 
