cbbda2cb345669c1200c74e10919f5072f922dd6
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2023-08-03 11:56:47 -0500

    Add files via upload

---
+++ quant_inf.py
@@ -0,0 +1,25 @@
+import os
+import pathlib
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+from pycoral.adapters import common
+from pycoral.adapters import classify
+
+# Specify the TensorFlow model, labels, and image
+script_dir = pathlib.Path(__file__).parent.absolute()
+model_file = os.path.join(script_dir, 'model_quant_8_full_edgetpu.tflite')
+
+# Initialize the TF interpreter
+interpreter = edgetpu.make_interpreter(model_file)
+interpreter.allocate_tensors()
+
+# Resize the image
+size = common.input_size(interpreter)
+row = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.8]
+
+# Run an inference
+common.set_input(interpreter, row)
+interpreter.invoke()
+classes = classify.get_scores(interpreter)
+
+print(f'classes: {classes}')
